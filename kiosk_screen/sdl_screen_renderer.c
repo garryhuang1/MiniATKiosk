4046c1d03e2025399d718d21796da4b555e022ed
Aaron <hunga@sunyit.edu>
2015-02-28 20:07:46 -0500

    Added SDL Screen Rendering Library

---
+++ kiosk_screen/sdl_screen_renderer.c
@@ -0,0 +1,214 @@
+/*  sdl_screen_renderer.c
+ *  Kiosk SDL Screen Rendering Implementation File
+ *  Author: Aaron Hung
+ *  Date: Feb. 28, 2015
+ *  Purpose: To render the Kiosk screen by initializng SDL assets and applying them to an independent SDL window.
+ *  Dependencies: SDL.h, SDL_image.h, stdbool.h, sdl-screen-1.00.h
+ */
+
+#include "sdl_screen_renderer.h"
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+/*
+ *  Function Prototypes
+ */
+int screen_init(int width, int height, const char* img_path);
+bool SDL_init();
+bool SDL_window_init(int width, int height);
+bool SDL_window_surface_init();
+bool SDL_load_screen_surface(const char* img_path);
+bool SDL_apply_screen_surface(int x, int y, SDL_Surface* src, SDL_Surface* dest);
+int screen_close();
+void error_handler(unsigned short error_code);
+
+/*
+ *  Error Code Constants
+ */
+const unsigned short SDL_INIT_ERROR = 0;
+const unsigned short SDL_WINDOW_ERROR = 1;
+const unsigned short SDL_WINDOW_SURFACE_ERROR = 2;
+const unsigned short SDL_IMG_PATH_ERROR = 3;
+const unsigned short SDL_IMG_OPTI_ERROR = 4;
+const unsigned short SDL_IMG_LOAD_ERROR = 5;
+const unsigned short SDL_SCREEN_BLIT_ERROR = 6;
+const unsigned short SDL_WINDOW_SURFACE_UPDATE_ERROR = 7;
+const unsigned short SDL_SCREEN_SURFACE_FREE_ERROR = 8;
+const unsigned short SDL_WINDOW_FREE_ERROR = 9;
+
+/*
+ *  SDL Assets
+ */
+SDL_Window* window = NULL;
+SDL_Surface* window_surface = NULL;
+SDL_Surface* screen_surface = NULL;
+
+
+int screen_init(int width, int height, const char* img_path) {
+	bool success;
+
+	success = SDL_init();
+
+	if (!success) {
+		error_handler(SDL_INIT_ERROR);
+	}
+
+	if (success) {
+		if (!SDL_window_init(width, height)) {
+			success = false;
+			error_handler(SDL_WINDOW_ERROR);
+		}
+	}
+
+	if (success) {
+		if (!SDL_window_surface_init(window_surface)) {
+			success = false;
+			error_handler(SDL_WINDOW_SURFACE_ERROR);
+		}
+	}
+
+	if (success) {
+		if (!SDL_load_screen_surface(img_path)) {
+			success = false;
+			error_handler(SDL_IMG_LOAD_ERROR);
+		}
+	}
+
+	if (success) {
+		if (!SDL_apply_screen_surface(0,0,screen_surface, window_surface)) {
+			success = false;
+			error_handler(SDL_SCREEN_BLIT_ERROR);
+		}
+	}
+
+	if (success) {
+		if (SDL_UpdateWindowSurface(window) < 0) {
+			success = false;
+			error_handler(SDL_WINDOW_SURFACE_UPDATE_ERROR);
+		}
+	}
+
+	if (success)
+		return 1;
+	else
+		return -1;
+}
+
+bool SDL_init() {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		return false;
+	}
+	return true;
+}
+
+bool SDL_window_init(int width, int height) {
+	window = SDL_CreateWindow("Kiosk-1.00", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
+	if (window == NULL) {
+		return false;
+	}
+	return true;
+}
+
+bool SDL_window_surface_init() {
+	window_surface = SDL_GetWindowSurface(window);
+	if (window_surface == NULL) {
+		return false;
+	}
+	return true;
+}
+
+bool SDL_load_screen_surface(const char* img_path) {
+	screen_surface = IMG_Load(img_path);
+
+	if (screen_surface == NULL) {
+		error_handler(SDL_IMG_PATH_ERROR);
+	}
+	else {
+		screen_surface = SDL_ConvertSurface(screen_surface, window_surface->format, NULL);
+		if (screen_surface == NULL) {
+			error_handler(SDL_IMG_OPTI_ERROR);
+		}
+	}
+
+	if (screen_surface == NULL) {
+		return false;
+	}
+	return true;
+}
+
+bool SDL_apply_screen_surface(int x, int y, SDL_Surface* src, SDL_Surface* dest) {
+	SDL_Rect offset;
+	offset.x = x;
+	offset.y = y;
+
+	if (SDL_BlitSurface(src, NULL, dest, &offset) < 0) {
+		return false;
+	}
+	return true;
+}
+
+int screen_close() {
+	int success = 1;
+
+	SDL_FreeSurface(screen_surface);
+	screen_surface = NULL;
+	if (screen_surface != NULL) {
+		success = -1;
+		error_handler(SDL_SCREEN_SURFACE_FREE_ERROR);
+	}
+
+	SDL_DestroyWindow(window);
+	window = NULL;
+	if (window != NULL) {
+		success = -1;
+		error_handler(SDL_WINDOW_FREE_ERROR);
+	}
+
+	if (success > 0) {
+		SDL_Quit();
+	}
+
+	return success;
+}
+
+void error_handler(unsigned short error_code) {
+	switch (error_code) {
+		case 0:
+			fputs("Error initializing SDL!", stderr);
+			break;
+		case 1:
+			fputs("Error creating SDL window!", stderr);
+			break;
+		case 2:
+			fputs("Error initialing SDL window surface!", stderr);
+			break;
+		case 3:
+			fputs("Error loading image from path!", stderr);
+			break;
+		case 4:
+			fputs("Error optimizing loaded image for screen surface!", stderr);
+			break;
+		case 5:
+			fputs("Error loading image for screen surface!", stderr);
+			break;
+		case 6:
+			fputs("Error blitting screen surface to window surface!", stderr);
+			break;
+		case 7:
+			fputs("Error updating window surface with blitted screen surface!", stderr);
+			break;
+		case 8:
+			fputs("Error freeing screen surface!", stderr);
+			break;
+		case 9:
+			fputs("Error destroying window!", stderr);
+			break;
+		default:
+			fputs("Unknown error code argument in error handler!", stderr);
+			break;
+	}
+}
---
+++ kiosk_screen/sdl_screen_renderer.h
@@ -0,0 +1,78 @@
+/*  sdl-screen-1.00.h
+ *
+ *  Kiosk's screen SDL initialization functions
+ *  Functions that are called to initialize the Kiosk's screen window and surface
+ */
+
+#ifndef SDL_SCREEN_RENDERER_H_
+#define SDL_SCREEN_RENDERER_H_
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+extern int screen_init(int width, int height, const char* img_path);
+/*
+ *  Renders Kiosk screen surface in SDL window instance with provided image argument and size
+ *  Parameters: int window width, int window height, string literal path to screen image
+ *  Return: int
+ *          (positive value, successful / negative value, unsuccessful)
+ */
+
+extern bool SDL_init();
+/*
+ *  Initializes SDL library
+ *  Parameters: None
+ *  Return: bool
+ *          (true, successful / false, unsuccessful)
+ */
+
+extern bool SDL_window_init(int width, int height);
+/*
+ *  Initializes SDL window that is centered with provided width and height
+ *  Parameters: int window width, int window height
+ *  Return: bool
+ *          (true, successful / false, unsuccessful)
+ */
+
+extern bool SDL_window_surface_init();
+/*
+ *  Initializes SDL window surface
+ *  Parameters: None
+ *  Return: bool
+ *          (true, successful / false, unsuccessful)
+ */
+
+extern bool SDL_load_screen_surface(const char* img_path);
+/*
+ *  Loads screen surface image and assigns it to SDL surface
+ *  Parameters: string literal path to image
+ *  Return: bool
+ *          (true, successful / false, unsuccessful)
+ */
+
+extern bool SDL_apply_screen_surface(int x, int y, SDL_Surface* src, SDL_Surface* dest);
+/*
+ *  Applies/Blits SDL screen surface to window surface
+ *  Parameters: int x_offset, int y_offset, SDL_Surface* screen_surface, SDL_Surface* window_surface
+ *  Return: bool
+ *          (true, successful / false, unsuccessful)
+ */
+
+extern int screen_close();
+/*
+ *  Deallocates Kiosk screen assets by setting them to NULL and closes SDL subsystems
+ *  Parameters: None
+ *  Return: int
+ *  		(positive, if successful / negative, if unsuccessful)
+ */
+
+extern void error_handler(unsigned short error_code);
+/*
+ *  Error hander function for debugging
+ *  Parameters: unsigned short error code constant
+ *  Return: Descriptive error notification string to standard error
+ */
+
+#endif /* SDL_SCREEN_RENDERER_H_ */
