3023cd4eae4572bba4fe1bc28d2afb5898f2b4f0
garryhuang1 <garryhuang1@yahoo.com>
2015-05-05 09:40:19 -0700

    Added documentation

--- p_kiosk_print_screen.h
+++ p_kiosk_print_screen.h
@@ -16,9 +16,58 @@ struct p_kiosk_print_screen{
 };
 
 typedef struct p_kiosk_print_screen p_kiosk_print_screen;
-
+/**
+ * Create a kiosk keypad
+ *
+ * @param p_sdl_data and address
+ *		The p_sdl_data struct and peripheral address
+ *
+ * @return
+ *		The receipt printer screen instance
+ */
 extern p_kiosk_print_screen* p_kiosk_print_screen_new(p_sdl_data* init_struct, m_uword keypad_start_address);
+
+/**
+ * Destroy instance of receipt printer screen
+ *
+ * @param p_kiosk_print_screen
+ *		Pointer to receipt printer instance
+ *
+ * @return
+ *		Freed memory from receipt printer instance
+ */
 extern void p_kiosk_print_screen_free(p_kiosk_print_screen *p);
+
+ /**
+ * Give the receipt printer screen a clock cycle
+ *
+ * @param p_kiosk_print_screen
+ *		Pointer to receipt printer screen instance
+ *
+ * @return
+ *		Screen is read for input
+ */
 extern void p_kiosk_print_screen_clock(p_kiosk_print_screen *p);
+
+/**
+ * Gets the bus state for the kiosk receipt printer screen
+ *
+ * @param p_kiosk_print_screen
+ *		Pointer to receeipt printer screen instance
+ *
+ * @return m_bus
+ *		Bus state of receipt printer peripheral
+ */
 extern m_bus p_kiosk_print_screen_get_bus(p_kiosk_print_screen *p);
+
+/**
+ * Sets the bus state for the receipt printer screen
+ *
+ * @param p_kiosk_print_screen, m_bus
+ *		Pointer to keypad instance and a bus state
+ *
+ * @return
+ *		Set bus state for receipt printer screen to bus arg
+ */
 extern void p_kiosk_print_screen_set_bus(p_kiosk_print_screen *p, m_bus bus);
+#endif
